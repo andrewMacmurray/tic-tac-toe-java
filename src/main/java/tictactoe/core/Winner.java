29852568c4e8408ca0d5869802dae6c503c3111b
andrewMacmurray <a.macmurray@icloud.com>
2018-04-30 15:56:59 +0100

    use function predicate for matching winning state

--- src/main/java/tictactoe/core/Winner.java
+++ src/main/java/tictactoe/core/Winner.java
@@ -2,6 +2,8 @@ package tictactoe.core;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Winner {
 
@@ -16,17 +18,17 @@ public class Winner {
     }
 
     private static boolean hasWon(PlayerSymbol player, Board board) {
-        for (int[] st: winningStates()) {
-            boolean matchesWinningState = Arrays
-                    .stream(st)
-                    .allMatch(i -> board.getTile(i).isTakenBy(player));
-            if (matchesWinningState) return true;
-        }
-        return false;
+        Predicate<Integer[]> isWinningState =
+                st -> Arrays
+                        .stream(st)
+                        .allMatch(i -> board.getTile(i).isTakenBy(player));
+        return Arrays
+                .stream(winningStates())
+                .anyMatch(isWinningState);
     }
 
-    private static int[][] winningStates () {
-        int[][] winStates = {
+    private static Integer[][] winningStates () {
+        Integer[][] winStates = {
                 {1, 2, 3}, {4, 5, 6}, {7, 8, 9},
                 {1, 4, 7}, {2, 5, 8}, {3, 6, 9},
                 {1, 5, 9}, {3, 5, 7}
