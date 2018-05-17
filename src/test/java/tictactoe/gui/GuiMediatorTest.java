b415991e232c0348a2355cb0cc3889b963bfbb36
andrewMacmurray <a.macmurray@icloud.com>
2018-05-17 12:26:27 +0100

    refactor gui mediator test to use helper methods

--- src/test/java/tictactoe/gui/GuiMediatorTest.java
+++ src/test/java/tictactoe/gui/GuiMediatorTest.java
@@ -1,6 +1,7 @@
 package tictactoe.gui;
 
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.junit.Test;
@@ -15,81 +16,67 @@ import static org.testfx.matcher.control.TextMatchers.hasText;
 public class GuiMediatorTest extends ApplicationTest {
 
     private GuiMediator guiMediator;
-    private Scene currentScene;
+    private Parent rootNode;
 
     @Override
     public void start(Stage stage) {
         GuiMediator guiMediator = new GuiMediator();
-        currentScene = guiMediator.getCurrentScene();
+        Scene scene = guiMediator.getCurrentScene();
         guiMediator.runGame();
+        rootNode = scene.getRoot();
 
-        stage.setScene(currentScene);
+        stage.setScene(scene);
         stage.show();
     }
 
     @Test
     public void clickOnTile() {
-        clickOn(lookupTile(0));
-
-        verifyThat(currentScene.getRoot(), hasChildren(1, ".player-x"));
-        verifyThat(currentScene.getRoot(), hasChildren(0, ".player-o"));
+        clickOnTile(0);
+        verifyThat(rootNode, hasChildren(1, ".player-x"));
+        verifyThat(rootNode, hasChildren(0, ".player-o"));
     }
 
     @Test
     public void makeMultipleMoves() {
-        clickOn(lookupTile(0));
-        clickOn(lookupTile(1));
-        clickOn(lookupTile(2));
-        clickOn(lookupTile(3));
-
-        verifyThat(currentScene.getRoot(), hasChildren(2, ".player-x"));
-        verifyThat(currentScene.getRoot(), hasChildren(2, ".player-o"));
+        clickOnTiles(0, 1, 2, 3);
+        verifyThat(rootNode, hasChildren(2, ".player-x"));
+        verifyThat(rootNode, hasChildren(2, ".player-o"));
     }
 
     @Test
     public void disabledClickOnExistingMove() {
-        clickOn(lookupTile(0));
-        clickOn(lookupTile(0));
-
-        verifyThat(currentScene.getRoot(), hasChildren(1, ".player-x"));
-        verifyThat(currentScene.getRoot(), hasChildren(0, ".player-o"));
+        clickOnTiles(0, 0);
+        verifyThat(rootNode, hasChildren(1, ".player-x"));
+        verifyThat(rootNode, hasChildren(0, ".player-o"));
     }
 
     @Test
     public void xWin() {
-        clickOn(lookupTile(0));
-        clickOn(lookupTile(3));
-        clickOn(lookupTile(1));
-        clickOn(lookupTile(4));
-        clickOn(lookupTile(2));
-
+        clickOnTiles(0, 3, 1, 4, 2);
         verifyThat(".status-text", hasText("Player X Won!"));
     }
 
     @Test
     public void oWin() {
-        clickOn(lookupTile(3));
-        clickOn(lookupTile(0));
-        clickOn(lookupTile(4));
-        clickOn(lookupTile(1));
-        clickOn(lookupTile(6));
-        clickOn(lookupTile(2));
-
+        clickOnTiles(3, 0, 4, 1, 6, 2);
         verifyThat(".status-text", hasText("Player O Won!"));
     }
 
     @Test
     public void disabledClicksAfterGame() {
-        clickOn(lookupTile(0));
-        clickOn(lookupTile(3));
-        clickOn(lookupTile(1));
-        clickOn(lookupTile(4));
-        clickOn(lookupTile(2));
+        clickOnTiles(0, 3, 1, 4, 2, 8);
+        verifyThat(rootNode, hasChildren(3, ".player-x"));
+        verifyThat(rootNode, hasChildren(2, ".player-o"));
+    }
 
-        clickOn(lookupTile(8));
+    private void clickOnTiles(int... tiles) {
+        for (int tileIndex : tiles) {
+            clickOnTile(tileIndex);
+        }
+    }
 
-        verifyThat(currentScene.getRoot(), hasChildren(3, ".player-x"));
-        verifyThat(currentScene.getRoot(), hasChildren(2, ".player-o"));
+    private void clickOnTile(int index) {
+        clickOn(lookupTile(index));
     }
 
     private Node lookupTile(int index) {
